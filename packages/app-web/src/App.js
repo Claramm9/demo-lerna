390c9b03c6089a5ad9b00460cdfca53a3aebc6a5
Clara M <claramm94@gmail.com>
2021-04-15 03:21:48 +0200

    add multiply

--- packages/app-web/src/App.js
+++ packages/app-web/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const basicsOperations = require('@testing-lerna/basics-operations');
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
     </header>
+    <span>Operation 2*2 equals {basicsOperations.multiply(2, 2)}</span>
   </div>
 );
 
